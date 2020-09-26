556a8028b3915c12ca36eb23078f4af6f694cec7
Hajnalka Judit Tóth <toth.hajni97@gmail.com>
2020-09-26 17:23:59 +0200

    FullName

--- UserMaintenance/Form1.cs
+++ UserMaintenance/Form1.cs
@@ -17,7 +17,7 @@ namespace UserMaintenance
         public Form1()
         {
             InitializeComponent();
-            label1.Text = Resource1.LastName; // label1
+            label1.Text = Resource1.FullName; // label1
             label2.Text = Resource1.FirstName; // label2
             button1.Text = Resource1.Add; // button1
 
@@ -31,8 +31,8 @@ namespace UserMaintenance
         {
             var u = new User()
             {
-                LastName = textBox1.Text,
-                FirstName = textBox2.Text
+                FullName = textBox1.Text,
+                
             };
             users.Add(u);
         }
