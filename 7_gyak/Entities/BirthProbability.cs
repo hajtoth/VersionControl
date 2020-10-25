3814ba40ca7550ffdd56475527d8acd19466a40f
Hajnalka Judit Tóth <toth.hajni97@gmail.com>
2020-10-25 16:46:31 +0100

    Crate BirthProbability class

---
+++ 7_gyak/Entities/BirthProbability.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _7_gyak.Entities
+{
+    public class BirthProbability
+    {
+        public byte Age { get; set; }
+        public int NbrOfChildren { get; set; }
+        public double BrthGvingProbability { get; set; }
+    }
+}
