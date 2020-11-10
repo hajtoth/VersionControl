4f3165bc9fe3497149aca208c1b1dd93266c25b0
Hajnalka Judit Tóth <toth.hajni97@gmail.com>
2020-11-10 10:13:03 +0100

    create new branch

---
+++ 7_gyak/Form1.Designer.cs
@@ -0,0 +1,145 @@
+﻿namespace _7_gyak
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.endyear_lbl = new System.Windows.Forms.Label();
+            this.endyear_nUD = new System.Windows.Forms.NumericUpDown();
+            this.population_lbl = new System.Windows.Forms.Label();
+            this.population_txt = new System.Windows.Forms.TextBox();
+            this.browse_btn = new System.Windows.Forms.Button();
+            this.start_btn = new System.Windows.Forms.Button();
+            this.result_txt = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.endyear_nUD)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // endyear_lbl
+            // 
+            this.endyear_lbl.AutoSize = true;
+            this.endyear_lbl.Location = new System.Drawing.Point(5, 12);
+            this.endyear_lbl.Name = "endyear_lbl";
+            this.endyear_lbl.Size = new System.Drawing.Size(58, 20);
+            this.endyear_lbl.TabIndex = 0;
+            this.endyear_lbl.Text = "Záróév";
+            // 
+            // endyear_nUD
+            // 
+            this.endyear_nUD.Location = new System.Drawing.Point(88, 12);
+            this.endyear_nUD.Maximum = new decimal(new int[] {
+            2005,
+            0,
+            0,
+            0});
+            this.endyear_nUD.Minimum = new decimal(new int[] {
+            2005,
+            0,
+            0,
+            0});
+            this.endyear_nUD.Name = "endyear_nUD";
+            this.endyear_nUD.Size = new System.Drawing.Size(120, 26);
+            this.endyear_nUD.TabIndex = 1;
+            this.endyear_nUD.Value = new decimal(new int[] {
+            2005,
+            0,
+            0,
+            0});
+            // 
+            // population_lbl
+            // 
+            this.population_lbl.AutoSize = true;
+            this.population_lbl.Location = new System.Drawing.Point(225, 12);
+            this.population_lbl.Name = "population_lbl";
+            this.population_lbl.Size = new System.Drawing.Size(105, 20);
+            this.population_lbl.TabIndex = 2;
+            this.population_lbl.Text = "Népesség fájl";
+            // 
+            // population_txt
+            // 
+            this.population_txt.Location = new System.Drawing.Point(341, 12);
+            this.population_txt.Name = "population_txt";
+            this.population_txt.Size = new System.Drawing.Size(100, 26);
+            this.population_txt.TabIndex = 3;
+            // 
+            // browse_btn
+            // 
+            this.browse_btn.Location = new System.Drawing.Point(484, 6);
+            this.browse_btn.Name = "browse_btn";
+            this.browse_btn.Size = new System.Drawing.Size(75, 35);
+            this.browse_btn.TabIndex = 4;
+            this.browse_btn.Text = "Browse";
+            this.browse_btn.UseVisualStyleBackColor = true;
+            // 
+            // start_btn
+            // 
+            this.start_btn.Location = new System.Drawing.Point(591, 6);
+            this.start_btn.Name = "start_btn";
+            this.start_btn.Size = new System.Drawing.Size(75, 35);
+            this.start_btn.TabIndex = 5;
+            this.start_btn.Text = "Start";
+            this.start_btn.UseVisualStyleBackColor = true;
+            // 
+            // result_txt
+            // 
+            this.result_txt.Location = new System.Drawing.Point(4, 47);
+            this.result_txt.Multiline = true;
+            this.result_txt.Name = "result_txt";
+            this.result_txt.Size = new System.Drawing.Size(734, 172);
+            this.result_txt.TabIndex = 6;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(804, 281);
+            this.Controls.Add(this.result_txt);
+            this.Controls.Add(this.start_btn);
+            this.Controls.Add(this.browse_btn);
+            this.Controls.Add(this.population_txt);
+            this.Controls.Add(this.population_lbl);
+            this.Controls.Add(this.endyear_nUD);
+            this.Controls.Add(this.endyear_lbl);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.endyear_nUD)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label endyear_lbl;
+        private System.Windows.Forms.NumericUpDown endyear_nUD;
+        private System.Windows.Forms.Label population_lbl;
+        private System.Windows.Forms.TextBox population_txt;
+        private System.Windows.Forms.Button browse_btn;
+        private System.Windows.Forms.Button start_btn;
+        private System.Windows.Forms.TextBox result_txt;
+    }
+}
+
---
+++ 7_gyak/Form1.cs
@@ -0,0 +1,200 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using _7_gyak.Entities;
+
+namespace _7_gyak
+{
+    public partial class Form1 : Form
+    {
+        string path;
+
+        public List<Person> Population { get; }
+        public List<BirthProbability> BirthProbabilities { get; }
+        public List<DeathProbability> DeathProbabilities { get; }
+
+        public Form1()
+        {
+            InitializeComponent();
+            Population = GetPopulation(@"C:\Temp\nép.csv");
+            BirthProbabilities = GetBirthProbabilities(@"C:\Temp\születés.csv");
+            DeathProbabilities = GetDeathProbabilities(@"C:\Temp\halál.csv");
+        }
+
+        private List<DeathProbability> GetDeathProbabilities(string path)
+        {
+            List<DeathProbability> deathProbabilities = new List<DeathProbability>();
+            var lines = File.ReadAllLines(path);
+            foreach (var line in lines)
+            {
+                string[] rawData = line.Split(';');
+                DeathProbability deathProbability = new DeathProbability()
+                {
+                    Age = int.Parse(rawData[0]),
+                    Gender = (Gender)Enum.Parse(typeof(Gender), rawData[1]),
+                    Probability = double.Parse(rawData[2])
+                };
+                deathProbabilities.Add(deathProbability);
+            }
+
+            return deathProbabilities;
+        }
+
+        private List<BirthProbability> GetBirthProbabilities(string path)
+        {
+            List<BirthProbability> birthProbabilities = new List<BirthProbability>();
+            var lines = File.ReadAllLines(path);
+            foreach (var line in lines)
+            {
+                string[] rawData = line.Split(';');
+                BirthProbability birthProbability = new BirthProbability()
+                {
+
+
+                    Age = int.Parse(rawData[0]),
+                    NbrOfChildren = int.Parse(rawData[1]),
+                    Probability = double.Parse(rawData[2])
+                };
+                birthProbabilities.Add(birthProbability);
+            }
+
+            return birthProbabilities;
+        }
+
+        public List<Person> GetPopulation(string csvpath)
+        {
+            List<Person> population = new List<Person>();
+
+            using (StreamReader sr = new StreamReader(csvpath, Encoding.Default))
+            {
+                while (!sr.EndOfStream)
+                {
+                    var line = sr.ReadLine().Split(';');
+                    population.Add(new Person()
+                    {
+                        BirthYear = int.Parse(line[0]),
+                        Gender = (Gender)Enum.Parse(typeof(Gender), line[1]),
+                        NbrOfChildren = int.Parse(line[2])
+                    });
+                }
+            }
+            return population;
+        }
+        public List<BirthProbability> GetBirthProbability(string csvpath)
+        {
+            List<BirthProbability> birth = new List<BirthProbability>();
+
+            using (StreamReader sr = new StreamReader(csvpath, Encoding.Default))
+            {
+                while (!sr.EndOfStream)
+                {
+                    var line = sr.ReadLine().Split(';');
+                    birth.Add(new BirthProbability()
+                    {
+                        Age = byte.Parse(line[0]),
+                        NbrOfChildren = int.Parse(line[1]),
+                        Probability = double.Parse(line[2]),
+                    });
+                }
+            }
+            return birth;
+        }
+
+        public List<DeathProbability> GetDeathProbability(string csvpath)
+        {
+            List<DeathProbability> death = new List<DeathProbability>();
+
+            using (StreamReader sr = new StreamReader(csvpath, Encoding.Default))
+            {
+                while (!sr.EndOfStream)
+                {
+                    var line = sr.ReadLine().Split(';');
+                    death.Add(new DeathProbability()
+                    {
+                        Gender = (Gender)Enum.Parse(typeof(Gender), line[0]),
+                        Age = byte.Parse(line[1]),
+                        Probability = double.Parse(line[2]),
+                    });
+                }
+            }
+            return death;
+        }
+        private void Browse_btn_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            if (ofd.ShowDialog() == DialogResult.OK)
+            {
+                population_txt.Text = ofd.FileName;
+                path = ofd.FileName;
+            }
+        }
+        private void Start_btn_Click(object sender, EventArgs e)
+        {
+            List<Person> People = new List<Person>();
+            List<BirthProbability> BirthProbabilities = new List<BirthProbability>();
+            List<DeathProbability> DeathProbabilities = new List<DeathProbability>();
+            People = GetPopulation(path);
+            BirthProbabilities = GetBirthProbability(@"C:\Temp\születés.csv");
+            DeathProbabilities = GetDeathProbability(@"C:\Temp\halál.csv");
+            StreamWriter sw = new StreamWriter("népesség.txt");
+
+            Random rng = new Random();
+
+            for (int year = 2005; year <= endyear_nUD.Value; year++)
+            {
+                //Végigmegyünk az összes személyen
+                for (int i = 0; i < People.Count; i++)
+                {
+                    Person person = People[i];
+                    if (!person.IsAlive) continue; //Ha halott, ugrunk a ciklus következõ lépésére
+                    byte age = (byte)(year - person.BirthYear);
+
+                    //Halál kezelése
+                    double pDeath = (from x in DeathProbabilities
+                                     where x.Gender == person.Gender && x.Age == age + 1
+                                     select x.Probability).First();
+                    if (rng.NextDouble() <= pDeath) person.IsAlive = false;
+
+                    //Születés kezelése -- csak nõk szülnek
+                    if (person.Gender == Gender.Female)
+                    {
+                        //Szülési valószínûség kikeresése
+                        double pBirth = (from x in BirthProbabilities
+                                         where x.Age == age
+                                         select x.Probability).FirstOrDefault();
+
+                        //Születik gyermek?
+                        if (rng.NextDouble() <= pBirth)
+                        {
+                            Person újszülött = new Person();
+                            újszülött.BirthYear = year;
+                            újszülött.NbrOfChildren = 0;
+                            újszülött.Gender = (Gender)(rng.Next(1, 3));
+                            People.Add(újszülött);
+                        }
+                    }
+                }
+
+                int nbrOfMales = (from x in People
+                                  where x.Gender == Gender.Male && x.IsAlive
+                                  select x).Count();
+                int nbrOfFemales = (from x in People
+                                    where x.Gender == Gender.Female && x.IsAlive
+                                    select x).Count();
+
+                result_txt.Text += "Szimulációs év: " + year + "\r \n Fiuk: " + nbrOfMales + "\r \n Lányok: " + nbrOfFemales + Environment.NewLine;
+                sw.WriteLine("Szimulációs év: " + year + "  Fiuk: " + nbrOfMales + "  Lányok: " + nbrOfFemales);
+
+            }
+            sw.Close();
+        }
+    }
+}
+
