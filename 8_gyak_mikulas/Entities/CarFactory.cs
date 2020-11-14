02a71e9f9fb41eec668ce916d20ac5d833b94154
Hajnalka Judit Tóth <toth.hajni97@gmail.com>
2020-11-14 14:12:59 +0100

    Creating CarFactory class

--- 8_gyak_mikulas/Entities/CarFactory.cs
+++ 8_gyak_mikulas/Entities/CarFactory.cs
@@ -3,10 +3,15 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using _8_gyak_mikulas.Abstractions;
 
 namespace _8_gyak_mikulas.Entities
 {
-    class CarFactory
+    public class CarFactory : IToyFactory
     {
+        public Toy CreateNew()
+        {
+            return new Car();
+        }
     }
 }
