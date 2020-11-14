57d08b8e3bf9cab6e7f9139292ef1152008c0a13
Hajnalka Judit Tóth <toth.hajni97@gmail.com>
2020-11-14 14:17:00 +0100

    Abstraction in conveyor belt

--- 8_gyak_mikulas/Form1.cs
+++ 8_gyak_mikulas/Form1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using _8_gyak_mikulas.Abstractions;
 using _8_gyak_mikulas.Entities;
 
 namespace _8_gyak_mikulas
@@ -18,10 +19,10 @@ namespace _8_gyak_mikulas
             InitializeComponent();
             Factory = new BallFactory();
         }
-        private List<Ball> _balls = new List<Ball>();
+        private List<Toy> _toys = new List<Toy>();
 
-        private BallFactory _factory;
-        public BallFactory Factory
+        private IToyFactory _factory;
+        public IToyFactory Factory
         {
             get { return _factory; }
             set { _factory = value; }
@@ -32,7 +33,7 @@ namespace _8_gyak_mikulas
         private void createTimer_Tick(object sender, EventArgs e)
         {
             var ball = Factory.CreateNew();
-            _balls.Add(ball);
+            _toys.Add(ball);
             ball.Left = -ball.Width;
             mainPanel.Controls.Add(ball);
         }
@@ -40,18 +41,18 @@ namespace _8_gyak_mikulas
         private void conveyorTimer_Tick(object sender, EventArgs e)
         {
             var maxPosition = 0;
-            foreach (var ball in _balls)
+            foreach (var ball in _toys)
             {
-                ball.MoveBall();
+                ball.MoveToy();
                 if (ball.Left > maxPosition)
                     maxPosition = ball.Left;
             }
 
             if (maxPosition > 1000)
             {
-                var oldestBall = _balls[0];
+                var oldestBall = _toys[0];
                 mainPanel.Controls.Remove(oldestBall);
-                _balls.Remove(oldestBall);
+                _toys.Remove(oldestBall);
             }
         }
 
