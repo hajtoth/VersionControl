50e5e8a952816e4beed71227228f9b4be038205e
Hajnalka Judit Tóth <toth.hajni97@gmail.com>
2020-11-14 14:11:30 +0100

    Modify BallFactory class

--- 8_gyak_mikulas/Entities/CarFactory.cs
+++ 8_gyak_mikulas/Entities/CarFactory.cs
@@ -3,15 +3,10 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using _8_gyak_mikulas.Abstractions;
 
 namespace _8_gyak_mikulas.Entities
 {
-    public class BallFactory : IToyFactory
+    class CarFactory
     {
-        public Toy CreateNew()
-        {
-            return new Ball();
-        }
     }
 }
