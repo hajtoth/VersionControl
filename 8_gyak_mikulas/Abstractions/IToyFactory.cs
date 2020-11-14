feb470b09e29fa6288d6d42c9d221ccc40b112df
Hajnalka Judit Tóth <toth.hajni97@gmail.com>
2020-11-14 14:07:39 +0100

    Add car.png and create IToyFactory

---
+++ 8_gyak_mikulas/Abstractions/IToyFactory.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _8_gyak_mikulas.Abstractions
+{
+    public interface IToyFactory
+    {
+        Toy CreateNew();
+    }
+}
