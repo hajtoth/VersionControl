5520522d8c2013d43f17f6ad2f005cf3677faae1
Hajnalka Judit Tóth <toth.hajni97@gmail.com>
2020-11-20 10:19:50 +0100

    New class (AccountControllerTestFixture) and new function (TestValidateEmail)

---
+++ UnitTestExample.Test/AccountControllerTestFixture.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace UnitTestExample.Test
+{
+    public class AccountControllerTestFixture
+    {
+        [Test]
+        public void TestValidateEmail(string email, bool expectedResult)
+        {
+            //A függvénynek legyen két bemenő paramétere
+            //email(string) - az eredeti függvény bemenetének megfelelően
+            //expectedResult(bool) - az eredeti függvény kimenetének megfelelően
+        }
+    }
+}
